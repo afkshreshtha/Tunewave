0ae2a88ce49032db02554eca12139df053670084
afkshreshtha <shreshthaagarwal1234@gmail.com>
2024-07-31 23:26:15 +0530

    add loading in homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,33 +1,57 @@
 'use client'
 
 import Image from 'next/image'
-import pop from '../public/pop.png'
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 import { HomePageData } from './constant'
 import { useRouter } from 'next/navigation'
+import { useState, useEffect } from 'react'
+
 const Discover = () => {
+  const [isLoading, setIsLoading] = useState(true)
   const router = useRouter()
+
+  useEffect(() => {
+    // Simulate a delay to show skeleton loaders
+    const timer = setTimeout(() => {
+      setIsLoading(false)
+    }, 1000) // Adjust the time as needed
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <>
       <div>
         <h3 className="text-white text-2xl font-bold">Discover your Genres</h3>
       </div>
-      <div className="text-white grid gap-1 grid-cols-2 md:grid-cols-3 lg:grid-cols-8 xl:gird-cols-10">
-        {HomePageData.map((data, index) => {
-          return (
-            <div className=" p-5 " key={index} >
-              <Image
-                full
-                width={180}
-                height={180}
-                className=" object-cover cursor-pointer"
-                src={data.image}
-                alt={data.name}
-                onClick={()=>router.push(`/genre/${data?.title}`)}
-              />
-              <h4 className="mt-4 text-xl uppercase cursor-pointer"onClick={()=>router.push(`/genre/${data.title}`)}>{data.title}</h4>
-            </div>
-          )
-        })}
+      <div className="text-white grid gap-1 grid-cols-2 md:grid-cols-3 lg:grid-cols-8 xl:grid-cols-10">
+        {isLoading
+          ? Array.from({ length: 8 }).map((_, index) => (
+              <div className="p-5" key={index}>
+                <SkeletonTheme baseColor="#343333">
+                  <Skeleton height={180} width={180} />
+                  <Skeleton height={20} width={120} className="mt-4" />
+                </SkeletonTheme>
+              </div>
+            ))
+          : HomePageData.map((data, index) => (
+              <div className="p-5" key={index}>
+                <Image
+                  width={180}
+                  height={180}
+                  className="object-cover cursor-pointer"
+                  src={data.image}
+                  alt={data.name}
+                  onClick={() => router.push(`/genre/${data?.title}`)}
+                />
+                <h4
+                  className="mt-4 text-xl uppercase cursor-pointer"
+                  onClick={() => router.push(`/genre/${data.title}`)}
+                >
+                  {data.title}
+                </h4>
+              </div>
+            ))}
       </div>
     </>
   )
